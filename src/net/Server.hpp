f1ac523a336f6d70e10611d9394db7caf89c5900
carneirofc <claudiofcarneiro@hotmail.com>
2020-04-01 17:23:41 -0300

    net: Socket maintenance and graceful shudown.

--- src/net/Server.hpp
+++ src/net/Server.hpp
@@ -8,18 +8,21 @@
 namespace Net{
     class Server{
         public:
-            Server(std::shared_ptr<Net::Handler> handler);
             Server(std::shared_ptr<Net::Handler> handler, const short unsigned int tcpPort);
-            Server(std::shared_ptr<Net::Handler> handler, const unsigned char* unixEndpoint);
+            Server(std::shared_ptr<Net::Handler> handler, const char* unixEndpoint);
+            ~Server();
             void listen();
+            void shutdown();
+            void stop();
 
         private:
             std::shared_ptr<Net::Handler> m_handler;
-            boost::asio::io_context m_IOContext;
-            boost::asio::generic::stream_protocol::socket m_Socket;
+            std::shared_ptr<boost::asio::io_context> m_IOContext;
+            std::shared_ptr<boost::asio::generic::stream_protocol::socket> m_Socket;
             std::shared_ptr<boost::asio::local::stream_protocol::acceptor> m_UNIXAcceptor;
             std::shared_ptr<boost::asio::ip::tcp::acceptor> m_TCPAcceptor;
             void write(const std::string& message);
             const std::string read();
+            bool m_Run;
     };
 }
