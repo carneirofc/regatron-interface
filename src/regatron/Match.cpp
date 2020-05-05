08696fc5f82e97acd6150da94206579588ad7095
carneirofc <claudiofcarneiro@hotmail.com>
2020-05-05 17:46:05 -0300

    Split Match class into .cpp and .hpp

---
+++ src/regatron/Match.cpp
@@ -0,0 +1,69 @@
+#include "regatron/Match.hpp"
+
+namespace Regatron {
+
+Match::Match(const std::string &commandString, const std::string &setFormat,
+             std::function<std::string()>       getHandle,
+             std::function<std::string(double)> setHandle)
+    : m_CommandString(commandString), m_SetFormat(setFormat),
+      m_GetPattern(fmt::format("{}\n", m_CommandString)),
+      m_SetPattern(fmt::format("{} {}\n", m_CommandString, m_SetFormat)),
+      m_GetHandleFunc(std::move(getHandle)),
+      m_SetHandleFunc(std::move(setHandle)) {}
+
+/** @note: get only constructor */
+Match::Match(const std::string &          commandString,
+             std::function<std::string()> getHandle)
+    : Match(commandString, "%lf", std::move(getHandle), nullptr) {}
+
+/** @note: set only constructor */
+Match::Match(const std::string &                commandString,
+             std::function<std::string(double)> setHandle)
+    : Match(commandString, "%lf", nullptr, std::move(setHandle)) {}
+
+CommandType Match::getCommandType(const std::string &message) {
+    if (message.starts_with(GET)) {
+        return CommandType::getCommand;
+    }
+    if (message.starts_with(SET)) {
+        return CommandType::setCommand;
+    }
+    return CommandType::invalidCommand;
+}
+
+std::optional<double> Match::handleSet(const char *message) const {
+    double data{0};
+    int    r = std::sscanf(message, m_SetPattern.c_str(), &data);
+    return r == 1 ? std::optional<double>{data} : std::nullopt;
+}
+
+std::optional<std::string> Match::handle(const std::string &message) const {
+    CommandType commandType;
+
+    std::optional<double> param;
+
+    if (m_GetHandleFunc != nullptr && message == m_GetPattern) {
+        commandType = CommandType::getCommand;
+
+    } else if (m_SetHandleFunc != nullptr &&
+               (param = handleSet(message.c_str()))) {
+        commandType = CommandType::setCommand;
+    } else {
+        commandType = CommandType::invalidCommand;
+    }
+
+    switch (commandType) {
+    default:
+        return {};
+
+    case CommandType::getCommand:
+        return {fmt::format("{} {}\n", m_CommandString, m_GetHandleFunc())};
+
+    case CommandType::setCommand:
+        return {fmt::format("{} {}\n", m_CommandString,
+                            m_SetHandleFunc(param.value()))};
+    }
+}
+
+} // namespace Regatron
+
