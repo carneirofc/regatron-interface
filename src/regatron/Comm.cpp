790c9bc496816393f1b14598b8c85df7c77f926f
carneirofc <claudiofcarneiro@hotmail.com>
2020-05-12 15:29:28 -0300

    Fix connect range checks and logging

--- src/regatron/Comm.cpp
+++ src/regatron/Comm.cpp
@@ -36,32 +36,36 @@ void Comm::InitializeDLL() {
 void Comm::connect() { connect(m_Port, m_Port); }
 void Comm::connect(int port) { connect(port, port); }
 void Comm::connect(int fromPort, int toPort) {
+    if(fromPort < 0 || toPort < 0 || fromPort > toPort){
+        auto message = fmt::format(R"(invalid port setting. From "{}" to "{}".)", fromPort, toPort).c_str();
+        throw CommException(message);
+    }
 
-    // search device connected via DIGI RealPort adapter -> /dev/ttyD00
-    // set search string example: "/dev/ttyS0", "/dev/ttyUSB0", "/dev/ttyD00"
-    if (DllSetSearchDevice2ttyDIGI() != DLL_SUCCESS) {
+    if (DllSetSearchDevice2ttyUSB() != DLL_SUCCESS) {
         throw CommException("failed to set digi string pattern");
-    }
+     }
 
-    if (fromPort == toPort) {
-        LOG_INFO(R"(searching DIGI RealPort device "/dev/ttyD{:02}")",
-                 fromPort);
-    } else {
-        LOG_INFO(
-            R"(searching DIGI RealPort device in range "/dev/ttyD{:02}" to "/dev/ttyD{:02}")",
-            fromPort, toPort);
-    }
+   if (fromPort == toPort) {
+       LOG_INFO(R"(searching DIGI RealPort device "/dev/ttyUSB{}")",
+               fromPort);
+   } else {
+       LOG_INFO(
+           R"(searching DIGI RealPort device in range "/dev/ttyUSB{}" to "/dev/ttyUSB{}")",
+           fromPort, toPort);
+   }
 
     // search device
-    usleep(SEARCH_SLEEP_SEC); // hack: while eth and rs232 at the same tc
-                              // device: wait 2 sec
+    DllSetCommTimeouts(40, 40); //use this function for VM or rs232 over ethernet
+    usleep(SEARCH_SLEEP_SEC);   // hack: while eth and rs232 at the same tc
+                                // device: wait 2 sec
 
-    if (DllSearchDevice(fromPort, toPort, &m_PortNrFound) != DLL_SUCCESS ||
+    if (DllSearchDevice(fromPort+1, toPort+1, &m_PortNrFound) != DLL_SUCCESS ||
+    //if (DllSearchDevice(1, 1, &m_PortNrFound) != DLL_SUCCESS ||
         m_PortNrFound == -1) {
         throw CommException(
             fmt::format(
-                R"(failed to connect to a device in range Port range "/dev/ttyD{:02}" to "/dev/ttyD{:02})",
-                fromPort, toPort)
+                R"(failed to connect to a device in range Port range "/dev/ttyUSB{}" to "/dev/ttyUSB{}", PortNrFound "{}".)",
+                fromPort, toPort, m_PortNrFound)
                 .c_str());
     }
 
