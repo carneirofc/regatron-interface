a66afde3f3df694753592b3f2df5ae32766623e4
carneirofc <claudiofcarneiro@hotmail.com>
2020-05-14 10:18:58 -0300

    Conan as system includes

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,4 +1,5 @@
 macro(run_conan)
+    set(CONAN_SYSTEM_INCLUDES ON)
     # Download automatically, you can also just copy the conan.cmake file
     if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
         message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
