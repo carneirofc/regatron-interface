d5d3481342edb3fb181ae88b4bc984f150027040
carneirofc <claudiofcarneiro@hotmail.com>
2020-08-14 08:36:48 -0300

    communication test script

--- scripts/client.py
+++ scripts/client.py
@@ -3,28 +3,29 @@ import time
 import socket
 
 messages = [
-    "get voltage\n",
-    "set voltage 100.0\n",
-    "get current\n",
-    "set current 200.0\n",
+    "getDebug\n",
+    "setDebug 1\n",
 ]
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def test():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ("localhost", 5001)
-s.connect(server_address)
-
-try:
-    while True:
+    # Connect the socket to the port where the server is listening
+    server_address = ("localhost", 20005)
+    s.connect(server_address)
+    try:
         # Send data
-        for m in messages:
-            message = m.encode("utf-8")
-            print("> {}".format(message))
-            s.sendall(message)
-            data = s.recv(128)
-            print("< {}".format(data))
-            time.sleep(1)
-finally:
-    print("closing socket")
-    s.close()
+        for i in range(100000):
+            for m in messages:
+                message = m.encode("utf-8")
+                ##print("> {}".format(message))
+                s.sendall(message)
+                data = s.recv(128)
+                ##print("< {}".format(data))
+    finally:
+        print("closing socket")
+        s.close()
+
+if __name__ == '__main__':
+    import timeit
+    print(timeit.timeit("test()", setup="from __main__ import test", number=1))
